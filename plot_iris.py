5e1448b8d71ce5d895b2218e4e7a9b40fb838eb7
Vian <vian@example.com>
2024-11-28 16:10:02 +0100

    add petal plot

--- plot_iris.py
+++ plot_iris.py
@@ -12,7 +12,7 @@ y = iris.target
 plt.figure(figsize=(12, 4))
 
 # Plot per i sepali
-plt.subplot(1, 2, 1)
+plt.subplot(2, 2, 1)
 for i in range(3):
     mask = y == i
     plt.scatter(X[mask, 0], X[mask, 1], label=iris.target_names[i])
@@ -22,8 +22,32 @@ plt.title('Distribuzione dei Sepali')
 plt.legend()
 
 # Plot per i petali
-
+plt.subplot(2, 2, 2)
+for i in range(3):
+    mask = y == i
+    plt.scatter(X[mask, 2], X[mask, 3], label=iris.target_names[i])
+plt.xlabel('Lunghezza petali')
+plt.ylabel('Larghezza petali')
+plt.title('Distribuzione dei petali')
+plt.legend()
 # Ora tocca a te! Prova a fare il plot dei petali!
+plt.subplot(2, 2, 3)
+for i in range(3):
+    mask = y == i
+    plt.scatter(X[mask, 0], X[mask, 2], label=iris.target_names[i])
+plt.xlabel('Lunghezza sepalo')
+plt.ylabel('Lunghezza petali')
+plt.title('Distribuzione dei petali')
+plt.legend()
+
+plt.subplot(2, 2, 4)
+for i in range(3):
+    mask = y == i
+    plt.scatter(X[mask, 1], X[mask, 3], label=iris.target_names[i])
+plt.xlabel('Larghezza petali')
+plt.ylabel('Larghezza sepalo')
+plt.title('Distribuzione dei petali')
+plt.legend()
 # Poi fai commit e push
 
 plt.show()
